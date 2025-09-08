0cc8786cc131613cde43a302c99d570cd225f70c
trulycaffeinated <tjeffrey2003@gmail.com>
2025-09-08 10:39:44 -0400

    vault backup: 2025-09-08 10:39:44

---
+++ 0 CPET561/Labs/TestProject/TestProject.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+ENTITY TestProject is
+port(
+	CLK 			: in std_logic;
+	OUT_VECTOR 	: out std_logic_vector(6 downto 0)
+);
+end entity TestProject;
+
+architecture arch of TestProject is
+begin
+
+
+end architecture arch;
