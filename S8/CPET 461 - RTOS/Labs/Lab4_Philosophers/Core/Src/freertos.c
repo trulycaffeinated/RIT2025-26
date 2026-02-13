4a596b42efbc5abe7eb4fbdf7fc3ecebfd36de3e
trulycaffeinated <tjeffrey2003@gmail.com>
2026-02-13 12:56:54 -0500

    vault backup: 2026-02-13 12:56:54

--- S8/CPET 461 - RTOS/Labs/Lab4_Philosophers/Core/Src/freertos.c
+++ S8/CPET 461 - RTOS/Labs/Lab4_Philosophers/Core/Src/freertos.c
@@ -95,7 +95,7 @@ void App_RTOS_Init(void){
 	// Make 8 Philosoper's (tasks)
 	for(int i = 0; i < NumPHIL; i++){
 		PhilosopherNum[i] = i;
-		osThreadNew(PhilosopherTask, i, &philAttr);
+		osThreadNew(PhilosopherTask, &PhilosopherNum[i], &philAttr);
 	}
 }
 
