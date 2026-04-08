7a3173c7811a6cd899cec5f320bba5584c2c3963
trulycaffeinated <tjeffrey2003@gmail.com>
2026-04-08 08:55:24 -0400

    vault backup: 2026-04-08 08:55:24

--- S8/CPET 461 - RTOS/Labs/Lab7_WorkerTasks/Core/Src/freertos.c
+++ S8/CPET 461 - RTOS/Labs/Lab7_WorkerTasks/Core/Src/freertos.c
@@ -44,7 +44,7 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-#define numTasks 6
+#define numTasks 3
 static int workerNum[numTasks];
 volatile int tasksDone = 0;
 uint32_t startTime = 0;
