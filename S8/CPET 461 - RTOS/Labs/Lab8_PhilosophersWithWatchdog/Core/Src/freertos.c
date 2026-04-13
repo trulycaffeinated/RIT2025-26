56b31964e81a515db106aa05cc5240dc1e60106c
trulycaffeinated <tjeffrey2003@gmail.com>
2026-04-13 12:47:50 -0400

    vault backup: 2026-04-13 12:47:50

---
+++ S8/CPET 461 - RTOS/Labs/Lab8_PhilosophersWithWatchdog/Core/Src/freertos.c
@@ -0,0 +1,198 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "cmsis_os.h"
+#include "print.h"
+#include <stdlib.h>
+#include <stdbool.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define NumPHIL 8
+const osThreadAttr_t defaultAttr = {
+    .name       = "defaultTask",
+    .stack_size = 1024 * 4,    // bytes (2 KB)
+    .priority   = osPriorityNormal
+};
+const osThreadAttr_t philAttr = {
+    .name       = "Philosopher",
+    .stack_size = 1024 * 4,   // bytes (4 KB)
+    .priority   = osPriorityLow
+};
+osSemaphoreId_t forkSemaphoreHandle[NumPHIL];
+const osSemaphoreAttr_t forkSemaphore_attributes = {
+		.name = "forkSemaphore"
+};
+static int PhilosopherNum[NumPHIL];
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+void App_RTOS_Init(void);
+
+static void startDefaultTask(void *argument);
+static void PhilosopherTask(void *argument);
+
+static bool GetLeftFork(int leftForkIndex);
+static bool GetRightFork(int rightForkIndex);
+static void putLeftFork(int leftForkIndex);
+static void putRightFork(int rightForkIndex);
+
+/* USER CODE END FunctionPrototypes */
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+void App_RTOS_Init(void){
+	Print_Init();
+
+	// Start Default Task - Print RTOS initialized, then close
+	osThreadNew(startDefaultTask, NULL, &defaultAttr);
+
+	// Make 8 forks for the 8 philosophers
+	for(int i = 0; i < NumPHIL; i++){
+		forkSemaphoreHandle[i] = osSemaphoreNew(1, 1, NULL);
+	}
+
+	// Make 8 Philosoper's (tasks)
+	for(int i = 0; i < NumPHIL; i++){
+		PhilosopherNum[i] = i;
+		osThreadNew(PhilosopherTask, &PhilosopherNum[i], &philAttr);
+	}
+}
+
+static void startDefaultTask(void *argument){
+	//Print_Line("RTOS Initialized");
+	osThreadExit();
+}
+
+static void PhilosopherTask(void *argument)
+{
+
+    int id = *(int*)argument; // Pull Philosopher ID from arbitrary argument
+	srand(id); // SEED Rand() with Philosopher ID - when removed, every philosopher has the same delays
+
+    int desiredLeftFork  = id;
+    int desiredRightFork = (id + NumPHIL - 1) % NumPHIL;
+
+    bool gotLeft  = false;
+    bool gotRight = false;
+
+    while (1) // Begin state machine
+    {
+
+    	// MAX Value for delay
+    	int ranMAX = 4001;
+
+        // Thinking state
+        Print_Line("Philosopher %d - Thinking...", id);
+        int ranNum = (rand() % ranMAX) + 1000;
+        //Print_Line("Delay : %d", ranNum); // DEBUG LINE | DELAY
+        osDelay(ranNum);
+
+        // Hungry state
+        Print_Line("Philosopher %d - Hungry...", id);
+        while (!(gotLeft && gotRight))
+        {
+
+        	gotLeft = false;
+        	gotRight = false;
+
+        	// Randomize which fork is picked up first
+            int randFork = rand() & 1;  // 0 or 1
+            if (randFork == 0) {
+                gotLeft  = GetLeftFork(desiredLeftFork);
+                gotRight = GetRightFork(desiredRightFork);
+            }
+            else {
+                gotRight = GetRightFork(desiredRightFork);
+                gotLeft  = GetLeftFork(desiredLeftFork);
+            }
+
+            // Check forks
+            if (gotLeft && !gotRight)
+            {
+                putLeftFork(desiredLeftFork);
+                gotLeft = false;
+            }
+
+            if (gotRight && !gotLeft)
+            {
+                putRightFork(desiredRightFork);
+                gotRight = false;
+            }
+
+            osDelay(10);
+        }
+
+        // Eat
+        Print_Line("Philosopher %d - do I eat for I am hungry? Or because the creator said so?",id);
+        ranNum = (rand() % ranMAX) + 1000;
+        //Print_Line("Delay : %d", ranNum); // DEBUG LINE | DELAY
+        osDelay(ranNum);
+        putLeftFork(desiredLeftFork);
+        putRightFork(desiredRightFork);
+    } // End State Machine
+
+    // Debug Section
+    //Print_Line("Philosopher #: %d | Desired LF : %d | Desired Right Fork : %d",
+    //           id, desiredLeftFork, desiredRightFork);
+    //osThreadExit();
+}
+
+
+static bool GetLeftFork(int leftForkIndex){
+	return(osSemaphoreAcquire(forkSemaphoreHandle[leftForkIndex], 0) == osOK);
+}
+
+static bool GetRightFork(int rightForkIndex){
+	return(osSemaphoreAcquire(forkSemaphoreHandle[rightForkIndex], 0) == osOK);
+}
+
+static void putLeftFork(int leftForkIndex){
+	osSemaphoreRelease(forkSemaphoreHandle[leftForkIndex]);
+}
+
+static void putRightFork(int rightForkIndex){
+	osSemaphoreRelease(forkSemaphoreHandle[rightForkIndex]);
+}
+/* USER CODE END Application */
+
---
+++ S8/CPET 461 - RTOS/Labs/Lab8_PhilosophersWithWatchdog/Lab 4 Plan.md
@@ -0,0 +1,146 @@
+# Lab 4 Plan
+![[Pasted image 20260213122238.png]]
+
+## Print Module
+"Provides a module (.h and .c files) which protects access to the UART with a binary
+semaphore and allows text strings to be printed to the UART through a module-defined
+function"
+
+Print Module will hold a ``Print_Line()`` function that will transmit to the UART after acquiring the semaphore.
+Will also need an Init to create the semaphore before ``Print_Line()`` is called.
+```C
+void Print_Init(void)
+{
+	uartPrintSem = newSema(args);
+}
+
+void Print_Line(const char *fmt, ...)
+{
+	char buffer[256];
+	
+	/*
+	arb argument stuff so I can pass %d, %i blah
+	*/
+	
+	Down(); 
+	HAL_UART_Transmit(/*line*/);
+	UP();
+}
+```
+
+## Philosophers
+The meat
+We have ONE Philosopher task, that gets created ``NumPHIL`` times, in this case 8. We will also have a fork Semaphore array of size 8. Each fork will be it's own semaphore.
+
+Each philosopher will identify which forks they want, then begin the state machine.
+```mermaid
+flowchart LR
+    A[Identify Forks] --> B[Thinking]
+    B --> C[Delay 1–5 sec]
+    C --> D[Hungry]
+    D --> E[Attempt to Get Left Fork and Right Fork]
+    E --> F{Both forks acquired?}
+    F -- Yes --> G[Eating]
+    F -- No --> H[Put down any acquired fork]
+    H --> I[Delay briefly]
+    I --> E
+    G --> J[Put down both forks]
+    J --> B
+```
+We will create an array of philosophers and pass that array as an argument to the new task.
+```C
+	for(int i = 0; i < 8; i++){
+		PhilosopherNum[i] = i;
+		osThreadNew(PhilosopherTask, &PhilosopherNum[i], &philAttr); // by passing &PhilosopherNum[i] we can access the array number of the philosopher in the philosopher task
+	}
+```
+
+Array number as ID in philosopher task
+```C
+void PhilosopherTask(void *argument){
+	int id = *(int*)argument; // this takes the value of &PhilosopherNum[i] that we passed to the new thread
+}
+```
+
+The state machine will look something like this
+```C
+while(1){
+	//Thinking
+	Print_Line("Thinking");
+	
+	//delay
+	osDelay(/*random numer*/);
+	
+	// Hungry
+	Print_line("Hungry");
+	while(/*doesn't have both forks*/){
+		
+		getRight()
+		getLeft()
+		
+		if(/*Got left and not right*/) {
+			/*put left down and set bool false*/
+		}
+		if(/*Got right and not left*/) {
+			/*put right down and set bool false*/
+		}
+		
+		osDelay(10);
+	}
+	
+	// Eating
+	Print_Line("Eating");
+	//delay
+	osDelay(/*random numer*/);
+	
+	putForkLeft();
+	putForkRight();
+}
+```
+
+Fork functions
+```C
+static bool GetLeftFork(int leftForkIndex){
+return(osSemaphoreAcquire(forkSemaphoreHandle[leftForkIndex], 0) == osOK);
+}
+
+static bool GetRightFork(int rightForkIndex){
+return(osSemaphoreAcquire(forkSemaphoreHandle[rightForkIndex], 0) == osOK);
+}
+
+static void putLeftFork(int leftForkIndex){
+osSemaphoreRelease(forkSemaphoreHandle[leftForkIndex]);
+}
+
+static void putRightFork(int rightForkIndex){
+osSemaphoreRelease(forkSemaphoreHandle[rightForkIndex]);
+}
+```
+
+
+Some things I noticed post lab
+1. Randomize which fork is attempted to be picked up first.
+   ```C
+int randFork = rand() & 1; // 0 or 1
+if (randFork == 0) {
+gotLeft = GetLeftFork(desiredLeftFork);
+gotRight = GetRightFork(desiredRightFork);
+}
+else {
+gotRight = GetRightFork(desiredRightFork);
+gotLeft = GetLeftFork(desiredLeftFork);
+}
+   ```
+   2. Seed the randomizer for each philosopher
+**When the randomizer is not seeded per philosopher - they will all have the same delay, and a pattern of who eats will occur**
+We can do this by giving the seed the id of the philosopher
+```C
+void PhilosopherTask(void *argument){
+	srand(id);
+	/*
+	do stuff
+	*/
+}
+```
+3. Adjusted the rand to go from 0 - 4001 + 1000 instead of 0-4 + 1
+4. From Dr. S - "Whenever you use a random number generator function, you always want to use the maximum possible range of values to ensure that the sequence is in fact random.  With a range of 0-4, the sequence could produce a large number of identical values in a row before the value changes "randomly".  With a large range that is much less likely.  Random number generators are only guaranteed to produce different values each time they are called if the full 32-bit number is used.  Once modulus is performed on the value, that guarantee not longer exists." - using ``rand() % something`` will cause the "randomness" to no longer be guaranteed
